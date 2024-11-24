05b621373072987a4d33314efb85059c5c070b54
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-11-24 19:39:38 -0300

    update car list

--- src/main/java/com/lucas/turbomotors/db/DataLoader.java
+++ src/main/java/com/lucas/turbomotors/db/DataLoader.java
@@ -16,16 +16,16 @@ public class DataLoader implements CommandLineRunner {
     private CarRepository carRepository;
 
     private List<Car> carList = List.of(
-            new Car(null, "A powerful sports car with a 5.2 V5.3 engine", "Ford", "Mustang", "Red", "5.4", "Gasoline"),
-            new Car(null, "A luxury SUV with advanced features", "Audi", "Q5.5", "Black", "5.6", "Diesel"),
-            new Car(null, "A reliable and compact sedan", "Toyota", "Corolla", "White", "5.7", "Hybrid"),
-            new Car(null, "A stylish electric car with impressive range", "Tesla", "Model 5.8", "Blue", "5.9", "Electric"),
-            new Car(null, "An off-road vehicle built for adventure", "Jeep", "Wrangler", "Green", "6.0", "Gasoline"),
-            new Car(null, "A high-performance sports coupe", "BMW", "M6.1", "Yellow", "6.2", "Gasoline"),
-            new Car(null, "A family-friendly minivan with modern amenities", "Honda", "Odyssey", "Silver", "6.3", "Gasoline"),
-            new Car(null, "A compact and efficient hatchback", "Volkswagen", "Golf", "Gray", "6.4", "Gasoline"),
-            new Car(null, "An elegant and powerful luxury sedan", "Mercedes-Benz", "E-Class", "White", "6.5", "Gasoline"),
-            new Car(null, "A rugged and durable pickup truck", "Chevrolet", "Silverado", "Black", "6.6", "Diesel")
+            new Car(null, "A powerful sports car with a 5.2 V5.3 engine", "Ford", "Mustang", "Red", "2021", "Gasoline"),
+            new Car(null, "A luxury SUV with advanced features", "Audi", "Q5", "Black", "2022", "Diesel"),
+            new Car(null, "A reliable and compact sedan", "Toyota", "Corolla", "White", "2020", "Hybrid"),
+            new Car(null, "A stylish electric car with impressive range", "Tesla", "Model 3", "Blue", "2023", "Electric"),
+            new Car(null, "An off-road vehicle built for adventure", "Jeep", "Wrangler", "Green", "2019", "Gasoline"),
+            new Car(null, "A high-performance sports coupe", "BMW", "M4", "Yellow", "2021", "Gasoline"),
+            new Car(null, "A family-friendly minivan with modern amenities", "Honda", "Odyssey", "Silver", "2022", "Gasoline"),
+            new Car(null, "A compact and efficient hatchback", "Volkswagen", "Golf", "Gray", "2020", "Gasoline"),
+            new Car(null, "An elegant and powerful luxury sedan", "Mercedes-Benz", "E-Class", "White", "2021", "Gasoline"),
+            new Car(null, "A rugged and durable pickup truck", "Chevrolet", "Silverado", "Black", "2022", "Diesel")
     );
 
     @Override
