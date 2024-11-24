861b2506f67d752dc5cead9440d544dc51f24640
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-11-24 19:40:48 -0300

    finished TODO: Implement the rest of the endpoints

--- src/main/java/com/lucas/turbomotors/car/CarController.java
+++ src/main/java/com/lucas/turbomotors/car/CarController.java
@@ -1,13 +1,14 @@
 package com.lucas.turbomotors.car;
 
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cars")
@@ -22,12 +23,43 @@ public class CarController {
         return carList;
     }
 
-    @GetMapping("2")
+    @GetMapping("/2")
     public List<Car> getCars2() {
         List<Car> carList = carRepository.findAll();
         return carList;
     }
 
-    // Todo: Implement the rest of the endpoints (POST, PUT, DELETE)
+    @PostMapping
+    public CarResponseDTO createCar(@RequestBody CarRequestDTO data){
+        Car carData = new Car(data);
+        carRepository.save(carData);
+
+        return new CarResponseDTO(carData);
+    }
+
+    @PutMapping("/{id}")
+    public CarResponseDTO updateCar(@RequestBody CarRequestDTO data, @PathVariable("id") Long id) {
+        Optional<Car> carOptional = carRepository.findById(id);
+        if (!carOptional.isPresent()) {
+            throw new IllegalStateException("Car not found");
+        }
+
+        Car updatedCar = carOptional.get();
+        BeanUtils.copyProperties(data, updatedCar);
+        carRepository.save(updatedCar);
+
+        return new CarResponseDTO(updatedCar);
+    }
+
+    @DeleteMapping("/{id}")
+    public CarResponseDTO deleteCar(@PathVariable("id") Long id) {
+        Optional<Car> carOptional = carRepository.findById(id);
+        if (!carOptional.isPresent()) {
+            throw new IllegalStateException("Car not found");
+        }
+        carRepository.deleteById(id);
+
+        return new CarResponseDTO(carOptional.get());
+    }
 
 }
