6982fb3cc6f43c91a16fe76815797ff979c569eb
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-11-24 01:51:09 -0300

    remove id from CarRequestDTO

--- src/main/java/com/lucas/turbomotors/car/CarRequestDTO.java
+++ src/main/java/com/lucas/turbomotors/car/CarRequestDTO.java
@@ -1,5 +1,5 @@
 package com.lucas.turbomotors.car;
 
-public record CarRequestDTO (Long id, String description, String brand, String model, String color, String year,
+public record CarRequestDTO (String description, String brand, String model, String color, String year,
                              String fuelType) {
 }
